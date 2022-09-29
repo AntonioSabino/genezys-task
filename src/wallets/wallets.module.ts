227c659975f7697b675d9e294cefd6c202e1b990
Antonio Sabino <aslimajr@outlook.com>
2022-09-28 21:24:43 -0300

    feat: inclusão da webhook utilizando httpModule

--- src/wallets/wallets.module.ts
+++ src/wallets/wallets.module.ts
@@ -4,11 +4,13 @@ import { WalletsController } from './wallets.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Wallet, WalletSchema } from './schema/wallet.schema';
 import { AuthModule } from 'src/auth/auth.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Wallet.name, schema: WalletSchema }]),
     AuthModule,
+    HttpModule,
   ],
   controllers: [WalletsController],
   providers: [WalletsService],
--- src/wallets/wallets.service.ts
+++ src/wallets/wallets.service.ts
@@ -4,21 +4,46 @@ import { Model } from 'mongoose';
 import { CreateWalletDto } from './dto/create-wallet.dto';
 import { Wallet, WalletDocument } from './schema/wallet.schema';
 import { createAccount, getBalance } from '../utils/web3';
+import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class WalletsService {
   constructor(
     @InjectModel(Wallet.name) private walletModel: Model<WalletDocument>,
+    private readonly httpService: HttpService,
   ) {}
 
-  async create({ userId }: CreateWalletDto): Promise<Wallet> {
+  async create({ userId, url }: CreateWalletDto): Promise<Wallet> {
     const walletAddress = createAccount();
 
     const createdWallet: WalletDocument = new this.walletModel({
       userId,
       walletAddress,
     });
-    return createdWallet.save();
+
+    //Webhook - fetch POST google.com/userId+wallet
+    console.log(
+      this.httpService
+        .post(
+          `${url}/${JSON.stringify({
+            userId,
+            walletAddress,
+          })}`,
+          {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          },
+        )
+        .subscribe(),
+    );
+
+    createdWallet.save();
+
+    return {
+      userId,
+      walletAddress,
+    };
   }
 
   async getById(userId: string) {
